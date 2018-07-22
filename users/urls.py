6b3d064116baa1590ff66f0b0e6bc930907625db
Alexander Lim <alexander.halim+github@gmail.com>
2018-07-22 12:11:08 +0800

    Add authentication module.

---
+++ users/urls.py
@@ -0,0 +1,14 @@
+"""Defines URL patterns for users"""
+
+
+from django.urls import path
+from django.contrib.auth.views import LoginView
+
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    # Login page
+    path(r'^login/$', LoginView.as_view(template_name='users/login.html'), name='login'),
+]
